c6fd8acb9bc276da3f4fc0da3b31c46f9cbac3e4
kschins <kasey.schindler@me.com>
2014-06-06 12:57:01 -0500

    Initial Commit

---
+++ Pick For MeTests/Pick_For_MeTests.swift
@@ -0,0 +1,35 @@
+//
+//  Pick_For_MeTests.swift
+//  Pick For MeTests
+//
+//  Created by Kasey Schindler on 6/6/14.
+//  Copyright (c) 2014 Kasey Schindler. All rights reserved.
+//
+
+import XCTest
+
+class Pick_For_MeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
